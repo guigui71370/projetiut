36a9b1e2be25fa8207fc72eca652ab3ba100676a
guillaume <guillaume.decarvalho71@gmail.com>
2018-11-07 17:39:04 +0100

    ajout de l'acitivité score

---
+++ app/src/main/java/com/example/cassa/entrainementprojettut/score/ScoreActivity.java
@@ -0,0 +1,22 @@
+package com.example.cassa.entrainementprojettut.score;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+
+import com.example.cassa.entrainementprojettut.R;
+import com.example.cassa.entrainementprojettut.gameUtils.GameActivity;
+
+public class ScoreActivity  extends GameActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+
+
+
+
+    }
+
+}
