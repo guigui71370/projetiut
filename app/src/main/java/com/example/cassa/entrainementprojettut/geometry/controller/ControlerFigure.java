8b3d780af761422bec29f5cdd7cadb8a12c0103b
BBBrice <43603875+BBBrice@users.noreply.github.com>
2018-12-19 22:42:42 +0100

    Update ControlerFigure.java

--- app/src/main/java/com/example/cassa/entrainementprojettut/geometry/controller/ControlerFigure.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/geometry/controller/ControlerFigure.java
@@ -1,20 +1,37 @@
 package com.example.cassa.entrainementprojettut.geometry.controller;
 
-import com.example.cassa.entrainementprojettut.geometry.figure.*;
+import com.example.cassa.entrainementprojettut.geometry.FactoryFigure;
+import com.example.cassa.entrainementprojettut.geometry.FactorySetGeometry;
+import com.example.cassa.entrainementprojettut.geometry.figure.Carre;
+import com.example.cassa.entrainementprojettut.geometry.figure.Cercle;
+import com.example.cassa.entrainementprojettut.geometry.figure.Figure;
+import com.example.cassa.entrainementprojettut.geometry.figure.Losange;
+import com.example.cassa.entrainementprojettut.geometry.figure.Parallelogramme;
+import com.example.cassa.entrainementprojettut.geometry.figure.Quadrilatere;
+import com.example.cassa.entrainementprojettut.geometry.figure.Rectangle;
+import com.example.cassa.entrainementprojettut.geometry.figure.Trapeze;
+import com.example.cassa.entrainementprojettut.geometry.figure.TriangleI;
+import com.example.cassa.entrainementprojettut.geometry.figure.TriangleIR;
+import com.example.cassa.entrainementprojettut.geometry.figure.TriangleQ;
+import com.example.cassa.entrainementprojettut.geometry.figure.TriangleR;
+import com.example.cassa.entrainementprojettut.geometry.setGeometry.I_SetGeometry;
 import com.example.cassa.entrainementprojettut.geometry.view.DrawingView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ControlerFigure {
 
     private Figure f;
     private Figure lastFigure;
+    private I_SetGeometry set;
+    FactoryFigure ff;
 
-    public ControlerFigure(){
-
+    public ControlerFigure(int levelChosen){
         f = null;
         lastFigure = null;
-
+        set = new FactorySetGeometry().createSetGeometry(levelChosen);
+        ff = new FactoryFigure();
     }
 
     public void updateDrawingView(DrawingView drawingView) {
@@ -25,51 +42,24 @@ public class ControlerFigure {
         }
         else f = generateFigure();
 
-
         drawingView.updateFigure(f);
         lastFigure = f;
     }
 
     private Figure generateFigure(){
-        Random r = new Random();
-        int i = r.nextInt(11);
-        if (i == 0){
-            f = new Carre();
-        }
-        else if (i == 1){
-            f = new Cercle();
-        }
-        else if (i == 2){
-            f = new Losange();
-        }
-        else if (i == 3){
-            f = new Parralellogramme();
-        }
-        else if (i == 4){
-            f = new Quadrilatere();
-        }
-        else if (i == 5){
-            f = new Rectangle();
-        }
-        else if (i == 6){
-            f = new Trapeze();
-        }
-        else if (i == 7){
-            f = new TriangleI();
-        }
-        else if (i == 8){
-            f = new TriangleIR();
-        }
-        else if (i == 9){
-            f = new TriangleQ();
-        }
-        else if (i == 10){
-            f = new TriangleR();
-        }
-        else f = null;
-
+        f = ff.createFigure();
         return f;
     }
 
+    public String getName(){
+        return f.getName();
+    }
 
+    public String getTruePropertie(){
+        return set.getTruePropertie(f);
+    }
+
+    public String getFalsePropertie(){
+        return set.getFalsePropertie(f);
+    }
 }
