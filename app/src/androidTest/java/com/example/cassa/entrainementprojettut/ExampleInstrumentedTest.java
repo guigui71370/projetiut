dabef6db9acc794c547d3de1196df6979c4dd233
guillaume <guillaume.decarvalho71@gmail.com>
2019-01-10 18:40:02 +0100

    coerction d'un bug

--- app/src/androidTest/java/com/example/cassa/entrainementprojettut/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cassa/entrainementprojettut/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.example.cassa.entrainementprojettut;
 
 import android.content.Context;
-/*import android.support.test.InstrumentationRegistry;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>*/
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
@@ -24,4 +24,3 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.cassa.entrainementprojettut", appContext.getPackageName());
     }
 }
-*/
