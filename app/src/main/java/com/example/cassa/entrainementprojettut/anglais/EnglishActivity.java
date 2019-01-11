af632fa5ffbc05711287a43d8580781fa1f59477
guillaume <guillaume.decarvalho71@gmail.com>
2019-01-11 18:24:56 +0100

    debut

---
+++ app/src/main/java/com/example/cassa/entrainementprojettut/anglais/EnglishActivity.java
@@ -0,0 +1,14 @@
+package com.example.cassa.entrainementprojettut.anglais;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.cassa.entrainementprojettut.R;
+
+public class EnglishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english);
+    }
+}
