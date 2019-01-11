dee9488588925eea2b1b237d7201491636518199
guillaume <guillaume.decarvalho71@gmail.com>
2019-01-11 23:52:40 +0100

    debut du layout

--- app/src/main/java/com/example/cassa/entrainementprojettut/anglais/EnglishActivity.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/anglais/EnglishActivity.java
@@ -1,14 +1,19 @@
 package com.example.cassa.entrainementprojettut.anglais;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Chronometer;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.example.cassa.entrainementprojettut.R;
+import com.example.cassa.entrainementprojettut.gameUtils.GameActivity;
 
-public class EnglishActivity extends AppCompatActivity {
-
+public class EnglishActivity extends GameActivity {
+    @BindView(R.id.chronometer2)
+    Chronometer time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english);
+        ButterKnife.bind(this);
     }
 }
