d6f0c272a2276f6af1f252c239088b87199b3618
guigui71370 <guillaume.decarvalho71@gmail.com>
2019-01-27 22:59:24 +0100

    corection ia lvl4

--- app/src/main/java/com/example/cassa/entrainementprojettut/connect4/ia/Ialevel4.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/connect4/ia/Ialevel4.java
@@ -9,6 +9,14 @@ public class Ialevel4 implements I_Ia {
     public int getColumn() {
         return column;
     }
+    private int depth=2;
+
+    private int score;
+    private boolean round;
+    private int iterations;
+    int rows = 6; // Height
+    int columnsboard = 7; // Width
+    int result;
 
     @Override
     public void calculateColumn(Plateau plateau) {
@@ -19,8 +27,12 @@ public class Ialevel4 implements I_Ia {
             joueur='r';
 
 
+        plateau.setColoruse(plateau.getCia());
+        int tab[]=maximizePlay(plateau,depth);
+        result=tab[0];
 
-        int column=plateau.conect4In1Turn(plateau.getCia());
+
+        /*int column=plateau.conect4In1Turn(plateau.getCia());
         int column2=plateau.conect4In1Turn(joueur);
         if(column!=-1){
             this.column=column;
@@ -44,7 +56,7 @@ public class Ialevel4 implements I_Ia {
                 i=verifNedonepasdepuisssan4(i, plateau);
 
             }
-        }
+        }*/
 
     }
 
@@ -60,6 +72,75 @@ public class Ialevel4 implements I_Ia {
             return 0;
         }
 
+    }
+
+
+
+
+    /**
+     * Algorithm
+     * Minimax principle
+     */
+    private int[] maximizePlay (Plateau board,int depth){
+        // Call score of our board
+        int score = board.score();
+
+        // Break
+        if (board.isFinished(depth, score)) return new int[]{-1, score};
+
+        // Column, Score
+        int max[] = {-1, -99999};
+
+        // For all possible moves
+        for (int column = 0; column < this.columnsboard; column++) {
+            Plateau new_board =new Plateau(board); // Create new board
+            //System.out.println("debug1 "+coloruse);
+            if (new_board.insertCheckersIa(column,board.getColoruse())!=-1) {
+
+                //System.out.println("debug2 "+coloruse);
+
+                this.iterations++; // Debug
+                System.out.println("iterations :"+iterations);
+
+                int next_move[] = minimizePlay(new_board, depth - 1); // Recursive calling
+
+                // Evaluate new move
+                if (max[0] == -1 || next_move[1] > max[1]) {
+                    max[0] = column;
+                    max[1] = next_move[1];
+
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private int[] minimizePlay(Plateau board, int depth) {
+        int score = board.score();
+
+        if (board.isFinished(depth, score)) return new int[]{-1, score};
+
+        // Column, score
+        int min[] = {-1, 99999};
+
+        for (int column = 0; column < this.columnsboard; column++) {
+            Plateau new_board =new Plateau(board); // Create new board
+
+            if (new_board.insertCheckersIa(column,board.getColoruse())!=-1) {
+
+                this.iterations++;
+                System.out.println("iterations :"+iterations);
+                int next_move[] = maximizePlay(new_board, depth - 1);
+
+                if (min[0] == -1 || next_move[1] < min[1]) {
+                    min[0] = column;
+                    min[1] = next_move[1];
+                }
+
+            }
+        }
+        return min;
     }
 }
 
